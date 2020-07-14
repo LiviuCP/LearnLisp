7aca73bff29f4345003fc6758709438faddff2b9
Liviu Popa <liviucst@gmail.com>
2020-07-14 21:35:09 +0300

    Created tests for prime numbers determination

--- Tests/mathfunctionstests.lisp
+++ Tests/mathfunctionstests.lisp
@@ -11,3 +11,14 @@
   (assert-equal (gCommonDiv 17 19) 1)
   (assert-equal (gCommonDiv -30 15) 15)
 )
+
+(define-test test-prime-numbers-in-interval
+  (assert-true (equalp (getPrimeNumbers 2) #(2)))
+  (assert-true (equalp (getPrimeNumbers 10) #(2 3 5 7)))
+  (assert-true (equalp (getPrimeNumbers 20 2) #(2 3 5 7 11 13 17 19)))
+  (assert-true (equalp (getPrimeNumbers 16 14) #()))
+  (assert-true (equalp (getPrimeNumbers 50 20) #(23 29 31 37 41 43 47)))
+  (assert-true (equalp (getPrimeNumbers 17 3) #(3 5 7 11 13 17)))
+  (assert-true (equalp (getPrimeNumbers 17) #(2 3 5 7 11 13 17)))
+  (assert-true (equalp (getPrimeNumbers 20 11) #(11 13 17 19)))
+)
