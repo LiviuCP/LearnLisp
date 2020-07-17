f4e0b6f75c249ee7bf1be166ff7e5b6f0af0ccd8
Liviu Popa <liviucst@gmail.com>
2020-07-17 21:27:08 +0300

    Created a function to capture int input w/o cond
    It was required to be able to capture raw integer input that can be any
    number. Also it was required to write a demonstrative app, namely
    averages.lisp

--- Libs/inputoutput.lisp
+++ Libs/inputoutput.lisp
@@ -1,3 +1,16 @@
+(defun requestIntInput(message)
+  (check-type message string)
+  (setq result nil)
+  (loop
+   (princ message)
+   (setq input (read-line))
+   (setq input (string-left-trim " " input))
+   (setq input (string-right-trim " " input))
+   (cond ((= (length input) 0) (return))
+	 ((isStringInteger input) (setq result (parse-integer input)) (return))
+	 (t (write-line "You haven't entered a valid number. Please try again"))))
+  (return-from requestIntInput result))
+
 ; condition should be a lambda function that contains a single param to which the converted value (result) is passed as arg, e.g. #'(lambda(val)(setq res (> val 0)))
 (defun requestIntInputWithCondition(message condition conditionErrorMessage)
   (check-type message string)
