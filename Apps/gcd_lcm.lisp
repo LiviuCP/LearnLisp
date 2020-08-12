e7b58aa7fee0151812c8a68c79b7a75573a23097
Liviu Constantin POPA <liviucst@gmail.com>
2020-08-12 20:13:13 +0300

    Performed additional refactoring to gcd_lcm.lisp

--- Apps/gcd_lcm.lisp
+++ Apps/gcd_lcm.lisp
@@ -3,12 +3,12 @@
 (load "../Libs/parse.lisp")
 
 (defun main()
-  (let ((first nil) (second nil)) ; declare both variables here to have a better overview
+  (let ((first) (second)) ; declare both variables here to have a better overview
     (setq first (requestIntInputWithCondition "Enter the first number: " #'(lambda(val)(setq isDifferent (/= val 0))) "The number should be different from 0. Please try again"))
     (when (not (null first))
       (setq second (requestIntInputWithCondition "Enter the second number: " #'(lambda(val)(setq isDifferent (/= val 0))) "The number should be different from 0. Please try again")))
     (if (not (null second))
-	(let ((gcd (gCommonDiv first second)) (lcm nil)) ; we will declare l.c.m. here to have a better overview
+	(let ((gcd (gCommonDiv first second)) (lcm)) ; we will declare l.c.m. here to have a better overview
 	  (format t "The greatest common divisor of ~d and ~d is: ~d~%" first second gcd)
           (when (= gcd 1)
 	    (write-line "The numbers are prime among each other!"))
