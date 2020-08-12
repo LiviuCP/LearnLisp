cc6dec913f82a19624c2181b4192cfe7ba597b15
Liviu Constantin POPA <liviucst@gmail.com>
2020-08-12 19:05:31 +0300

    Refactored checkisintegerorfloat.lisp

--- Apps/checkisintegerorfloat.lisp
+++ Apps/checkisintegerorfloat.lisp
@@ -4,13 +4,13 @@
   (screen:clear-window (screen:make-window))
   (loop
    (princ "Enter an integer number from keyboard (press ENTER to exit): ")
-   (setq input (read-line))
-   (setq input (string-left-trim " " input))                                ; spaces from the left side don't count
-   (setq input (string-right-trim " " input))                               ; neither the ones from right
-   (cond ((= (length input) 0) (write-line "You exited") (return))
-	 ((convertStringToInt input) (write-line "The entered string is an integer value"))
-	 ((convertStringToFloat input) (write-line "The entered string is a float value"))
-	 (t (write-line "The entered string is neither an integer nor a float value")))
+   (let ((input (read-line)))
+     (setq input (string-left-trim " " input))                                ; spaces from the left side don't count
+     (setq input (string-right-trim " " input))                               ; neither the ones from right
+     (cond ((= (length input) 0) (write-line "You exited") (return))
+	   ((convertStringToInt input) (write-line "The entered string is an integer value"))
+	   ((convertStringToFloat input) (write-line "The entered string is a float value"))
+	   (t (write-line "The entered string is neither an integer nor a float value"))))
    (terpri)))
 
 (main)
