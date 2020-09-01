4780b4949801ecf04a6ea86408d50096b8c2af49
Liviu Popa <liviucst@gmail.com>
2020-09-01 19:48:02 +0300

    Refactored all input-output functions
    It was required to refactor all variables in order to be consistent with
    the LISP naming standards

--- Libs/inputoutput.lisp
+++ Libs/inputoutput.lisp
@@ -16,11 +16,11 @@
     (return-from request-integer-input result)))
 
 ; condition should be a lambda function that contains a single param to which the converted value (result) is passed as arg, e.g. #'(lambda(val)(setq res (> val 0)))
-(defun request-integer-input-with-condition(message condition conditionErrorMessage)
+(defun request-integer-input-with-condition(message condition condition-error-message)
   "This function is used for taking over integer user input that satisfies a specific condition. Exit criteria is user abort or entering an integer that satisfies the provided condition."
   (check-type message string)
   (check-type condition function)
-  (check-type conditionErrorMessage string)
+  (check-type condition-error-message string)
   (let ((result))
     (loop
      (princ message)
@@ -28,14 +28,14 @@
        (setq input (string-left-trim " " input))
        (setq input (string-right-trim " " input))
        (cond ((= (length input) 0) (return))
-	     (t (let ((tempResult (convert-string-to-integer input)))
-		  (cond ((not (null tempResult))
-			 (if (funcall condition tempResult)
+	     (t (let ((temp-result (convert-string-to-integer input)))
+		  (cond ((not (null temp-result))
+			 (if (funcall condition temp-result)
 			     (progn
-			       (setq result tempResult)
+			       (setq result temp-result)
 			       (return))
 			   (progn
-			     (princ conditionErrorMessage)
+			     (princ condition-error-message)
 			     (terpri))))
 			(t (write-line "You haven't entered a valid number. Please try again"))))))))
     (return-from request-integer-input-with-condition result)))
